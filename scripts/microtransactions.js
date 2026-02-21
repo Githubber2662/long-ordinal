d072dd56441560fe05b09098db623b342d3f0a06
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-02-21 12:24:25 +0200

    Reduced costs for microtransaction upgrades

--- scripts/microtransactions.js
+++ scripts/microtransactions.js
@@ -2,20 +2,20 @@
 function mtxUpg(n) {
   switch (n) {
     case 0:
-      if (game.mtx.money >= 25) {
-        game.mtx.money -= 25
+      if (game.mtx.money >= 20) {
+        game.mtx.money -= 20
         game.mtx.doublerTicks += 120 * 1000
       }
       break
     case 1:
-      if (game.mtx.money >= 60) {
-        game.mtx.money -= 60
+      if (game.mtx.money >= 30) {
+        game.mtx.money -= 30
         game.lastTick -= 60 * 1000
       }
       break
     case 2:
-      if (game.mtx.money >= 1000) {
-        game.mtx.money -= 1000
+      if (game.mtx.money >= 480) {
+        game.mtx.money -= 480
         
         if (game.ord.b > 60) game.ord.b = Math.ceil(game.ord.b * 0.95)
         else game.mtx.doubleIncs = true
